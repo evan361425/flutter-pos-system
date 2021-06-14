6865495678d25bc1d607f30640426cc0196f2d82
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-06-14 17:41:39 +0800

    build(test): currency_provider

--- lib/providers/currency_provider.dart
+++ lib/providers/currency_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:possystem/helpers/logger.dart';
 import 'package:possystem/services/cache.dart';
 
 class CurrencyProvider extends ChangeNotifier {
@@ -6,16 +7,20 @@ class CurrencyProvider extends ChangeNotifier {
 
   static const defaultCurrency = '新台幣';
 
-  static const candidates = {
+  static const currencyCandidates = {
     '新台幣': [1, 5, 10, 50, 100, 500, 1000],
+    'USD': [0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10, 20, 50, 100],
   };
 
   late String _currency;
 
-  late List<num> types;
+  /// Current available unit of money
+  late List<num> unitList;
 
+  /// Is this currency all int?
   late bool isInt;
 
+  /// Index of integer in [unitList]
   late int intIndex;
 
   CurrencyProvider() {
@@ -24,21 +29,33 @@ class CurrencyProvider extends ChangeNotifier {
 
   String get currency => _currency;
 
-  /// if value = 24 => 25
-  /// if value = 27 => 30
-  /// if value = 30 => 50
-  /// if value = 100 => 500
+  /// Ceiling [value] to currency least value
+  ///
+  /// TODO: make double ceiling to float point
+  ///
+  /// ```dart
+  /// // types = [5, 10 ,50];
+  /// ceil(3) == 5
+  /// ceil(6) == 10
+  /// ceil(11) == 15
+  /// ceil(25) == 30
+  /// ceil(30) == 50
+  /// ceil(60) == 100
+  /// ```
   num? ceil(num? value) {
     if (value == null) return null;
 
+    if (value < 0) return 0;
+
+    // if it is double ceil to int first
     if (value != value.ceil()) return value.ceil();
 
-    final nextIndex = types.indexWhere((e) => e > value);
-    if (nextIndex == 0 || nextIndex == 1) return types[nextIndex];
+    final next = unitList.indexWhere((e) => e > value);
+    if (next == 0 || next == 1) return unitList[next];
 
-    final useTypes = types.sublist(1, nextIndex == -1 ? null : nextIndex + 1);
-    for (var type in useTypes) {
-      if (value % type != 0) return (value / type).ceil() * type;
+    final useUnits = unitList.sublist(1, next == -1 ? null : next + 1);
+    for (var unit in useUnits) {
+      if (value % unit != 0) return (value / unit).ceil() * unit;
     }
 
     return value;
@@ -46,32 +63,38 @@ class CurrencyProvider extends ChangeNotifier {
 
   void initialize() {
     final currency = Cache.instance.get<String>(Caches.currency_code);
-    _setCurrency(currency ?? defaultCurrency);
+    if (!_setCurrency(currency)) {
+      _setCurrency(defaultCurrency);
+    }
   }
 
   String numToString(num value) {
     return isInt ? value.round().toString() : value.toString();
   }
 
-  Future<void> setUsage(String value) async {
+  Future<void> setCurrency(String value) async {
+    info(value, 'setting.currency');
     await Cache.instance.set<String>(Caches.currency_code, value);
-    if (_setCurrency(value)) {
+
+    if (value != _currency && _setCurrency(value)) {
       notifyListeners();
     }
   }
 
   bool _setCurrency(String? value) {
-    if (value == null || value == _currency) return false;
-    types = candidates[value]!;
+    if (value == null) return false;
+
+    if (currencyCandidates[value] == null) return false;
+
+    unitList = currencyCandidates[value]!;
 
     // index when money start using int
     intIndex = 0;
-    for (var type in types) {
-      if (type.toInt() == type) break;
+    for (var money in unitList) {
+      if (money.toInt() == money) break;
       intIndex++;
     }
 
-    // is this currency all int?
     isInt = intIndex == 0;
 
     _currency = value;
