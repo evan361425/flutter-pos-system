e976d1286fcbe712d888ce2fe59b824496055e93
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-06-24 19:01:51 +0800

    build(init): replace LoadMore by SmartRefresher

--- lib/ui/analysis/widgets/order_list.dart
+++ lib/ui/analysis/widgets/order_list.dart
@@ -1,38 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:loadmore/loadmore.dart';
+import 'package:flutter/rendering.dart';
+import 'package:possystem/components/meta_block.dart';
 import 'package:possystem/components/style/circular_loading.dart';
 import 'package:possystem/components/style/custom_styles.dart';
-import 'package:possystem/components/meta_block.dart';
 import 'package:possystem/constants/constant.dart';
-import 'package:possystem/helpers/logger.dart';
 import 'package:possystem/models/objects/order_object.dart';
 import 'package:possystem/providers/currency_provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'order_modal.dart';
 
 class OrderList extends StatefulWidget {
-  OrderList({Key? key, required this.handleLoad}) : super(key: key);
-
   final Future<List<OrderObject>> Function(Map<String, Object>, int) handleLoad;
+
+  OrderList({Key? key, required this.handleLoad}) : super(key: key);
 
   @override
   OrderListState createState() => OrderListState();
 }
 
 class OrderListState extends State<OrderList> {
+  final _scrollController = RefreshController();
+
   final List<OrderObject> _data = [];
   late Map<String, Object> _params;
-  num totalPrice = 0;
-  OrderListStatus _status = OrderListStatus.concealing;
-  bool isFinish = false;
+  bool? _isLoading;
 
-  set status(OrderListStatus status) => setState(() => _status = status);
+  num totalPrice = 0;
 
   @override
   Widget build(BuildContext context) {
-    if (_status == OrderListStatus.loading) {
+    if (_isLoading == true) {
       return CircularLoading();
-    } else if (_status == OrderListStatus.concealing) {
+    } else if (_isLoading == null) {
       return Text('點擊日期來查看紀錄', style: Theme.of(context).textTheme.muted);
     } else if (_data.isEmpty) {
       return Text('本日無點餐紀錄', style: Theme.of(context).textTheme.muted);
@@ -46,23 +46,30 @@ class OrderListState extends State<OrderList> {
           child: Text('總收入：$revenue', style: Theme.of(context).textTheme.muted),
         ),
         Expanded(
-          child: LoadMore(
-              onLoadMore: _handleLoad,
-              isFinish: isFinish,
-              textBuilder: (status) {
-                switch (status) {
-                  case LoadMoreStatus.idle:
-                    return 'Loading...';
-                  case LoadMoreStatus.loading:
-                    return '...';
-                  case LoadMoreStatus.fail:
-                    return 'failed';
-                  case LoadMoreStatus.nomore:
-                    return 'finish';
-                }
-              },
+          child: SmartRefresher(
+              controller: _scrollController,
+              enablePullUp: true,
+              enablePullDown: false,
+              onLoading: () => _handleLoad(),
+              footer: CustomFooter(
+                height: 30,
+                onClick: () => setState(() {
+                  _data.clear();
+                  _isLoading = true;
+                }),
+                builder: (BuildContext context, LoadStatus? mode) {
+                  if (mode == LoadStatus.loading) {
+                    return CircularLoading();
+                  } else if (mode == LoadStatus.failed) {
+                    Center(child: Text('糟糕！發生錯誤了'));
+                  } else if (mode == LoadStatus.canLoading) {
+                    return Center(child: Text('放開以讀取更多'));
+                  }
+                  return Container();
+                },
+              ),
               child: ListView.builder(
-                itemBuilder: (context, index) => _orderTile(_data[index]),
+                itemBuilder: (context, index) => _itemBuilder(_data[index]),
                 itemCount: _data.length,
               )),
         ),
@@ -72,32 +79,26 @@ class OrderListState extends State<OrderList> {
 
   void reset(Map<String, Object> params, num totalPrice) {
     this.totalPrice = totalPrice;
-    _params = params;
-    _data.clear();
-    status = OrderListStatus.loading;
+    setState(() {
+      _params = params;
+      _data.clear();
+      _isLoading = true;
+    });
     _handleLoad();
   }
 
-  Future<bool> _handleLoad() async {
-    try {
-      final data = await widget.handleLoad(_params, _data.length);
-      _data.addAll(data);
+  Future<void> _handleLoad() async {
+    final data = await widget.handleLoad(_params, _data.length);
 
-      setState(() {
-        _status = OrderListStatus.revealing;
-        isFinish = data.isEmpty;
-      });
+    _data.addAll(data);
+    data.isEmpty
+        ? _scrollController.loadNoData()
+        : _scrollController.loadComplete();
 
-      return true;
-    } catch (e) {
-      error(e.toString(), 'analysis.load.error');
-      return false;
-    }
+    setState(() => _isLoading = false);
   }
 
-  void hide() => status = OrderListStatus.concealing;
-
-  Widget _orderTile(OrderObject order) {
+  Widget _itemBuilder(OrderObject order) {
     final title = order.products.map<String>((e) {
       final count = e.count > 1 ? ' * ${e.count}' : '';
       return '${e.productName}$count';
