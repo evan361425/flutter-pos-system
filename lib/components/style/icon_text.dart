8eb3248152ead6f24ce4baa5f9202de29b81759e
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-06-30 18:26:50 +0800

    use icon data instead of icon

--- lib/components/style/icon_text.dart
+++ lib/components/style/icon_text.dart
@@ -2,33 +2,37 @@ import 'package:flutter/material.dart';
 
 class IconText extends StatelessWidget {
   final String text;
-  final String iconName;
+  final IconData icon;
   final TextStyle? textStyle;
 
   const IconText({
     Key? key,
     required this.text,
-    required this.iconName,
+    required this.icon,
     this.textStyle,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final style = textStyle ?? Theme.of(context).textTheme.bodyText1;
-    return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
-        style: style,
-        children: [
-          TextSpan(
-            text: iconName,
-            style: style?.copyWith(fontFamily: 'MaterialIcons'),
-          ),
-          TextSpan(
-            text: ' $text',
-          )
-        ],
-      ),
-    );
+    return Row(children: [
+      Icon(icon, color: textStyle?.color, size: textStyle?.fontSize ?? 14),
+      Text(' $text', style: textStyle),
+    ]);
+    // final style = textStyle ?? Theme.of(context).textTheme.bodyText1;
+    // return RichText(
+    //   textAlign: TextAlign.center,
+    //   text: TextSpan(
+    //     style: style,
+    //     children: [
+    //       TextSpan(
+    //         text: icon,
+    //         style: style?.copyWith(fontFamily: 'MaterialIcons'),
+    //       ),
+    //       TextSpan(
+    //         text: ' $text',
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 }
