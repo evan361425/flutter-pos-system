f03b8fb89ef19131f68f45cc0c217f0e1d44cfcd
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-07-25 11:47:12 +0800

    test: fix lint

--- test/services/cache_test.dart
+++ test/services/cache_test.dart
@@ -5,40 +5,45 @@ import 'package:possystem/services/cache.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../mocks/mock_cache.dart';
+import 'cache_test.mocks.dart';
 
 @GenerateMocks([SharedPreferences])
 void main() {
-  // group('#needTutorial', () {
-  // test('should return false if set', () {
-  //   when(service.getBool(any)).thenReturn(false);
+  group('#needTutorial', () {
+    test('should return false if set', () {
+      final service = MockSharedPreferences();
+      Cache.instance.service = service;
+      when(service.getBool(any)).thenReturn(false);
 
-  //   // get false should return false
-  //   expect(Cache.instance.needTutorial('some-key'), equals(false));
+      // get false should return false
+      expect(Cache.instance.needTutorial('some-key'), equals(false));
 
-  //   when(service.getBool(any)).thenReturn(true);
+      when(service.getBool(any)).thenReturn(true);
 
-  //   // get true should return false
-  //   expect(Cache.instance.needTutorial('some-key'), equals(false));
-  // });
+      // get true should return false
+      expect(Cache.instance.needTutorial('some-key'), equals(false));
+    });
 
-  // test('should return true if not set', () {
-  //   when(service.getBool(any)).thenReturn(null);
-  //   when(service.setBool(any, any)).thenAnswer((_) => Future.value(false));
+    test('should return true if not set', () {
+      final service = MockSharedPreferences();
+      Cache.instance.service = service;
+      when(service.getBool(any)).thenReturn(null);
+      when(service.setBool(any, any)).thenAnswer((_) => Future.value(false));
 
-  //   // Action
-  //   final result = Cache.instance.needTutorial('some-key');
+      // Action
+      final result = Cache.instance.needTutorial('some-key');
 
-  //   // Assertion
-  //   expect(result, equals(true));
-  //   verify(service.setBool(any, any)).called(1);
-  // });
-  // });
+      // Assertion
+      expect(result, equals(true));
+      verify(service.setBool(any, any)).called(1);
+    });
+  });
 
   setUp(() {
     Cache.instance = Cache();
   });
 
   tearDown(() {
-    // Cache.instance = cache;
+    Cache.instance = cache;
   });
 }
