38520a24d8efe0be244bddf8b5fb8302b59987dc
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-07-25 14:07:46 +0800

    feat(comp): Wrap text dialog to scroll view

--- lib/components/dialog/single_text_dialog.dart
+++ lib/components/dialog/single_text_dialog.dart
@@ -27,33 +27,35 @@ class _SingleTextDialogState extends State<SingleTextDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: widget.title,
-      content: SingleChildScrollView(
-        child: Form(
-          key: form,
-          child: TextFormField(
-            controller: textController,
-            autofocus: true,
-            onSaved: onSubmit,
-            onFieldSubmitted: onSubmit,
-            keyboardType: widget.keyboardType,
-            validator: widget.validator,
-            decoration: widget.decoration,
-            textInputAction: TextInputAction.done,
+    return SingleChildScrollView(
+      child: AlertDialog(
+        title: widget.title,
+        content: SingleChildScrollView(
+          child: Form(
+            key: form,
+            child: TextFormField(
+              controller: textController,
+              autofocus: true,
+              onSaved: onSubmit,
+              onFieldSubmitted: onSubmit,
+              keyboardType: widget.keyboardType,
+              validator: widget.validator,
+              decoration: widget.decoration,
+              textInputAction: TextInputAction.done,
+            ),
           ),
         ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text(tt('cancel')),
+          ),
+          ElevatedButton(
+            onPressed: () => onSubmit(textController.text),
+            child: Text(tt('confirm')),
+          ),
+        ],
       ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text(tt('cancel')),
-        ),
-        ElevatedButton(
-          onPressed: () => onSubmit(textController.text),
-          child: Text(tt('confirm')),
-        ),
-      ],
     );
   }
 
