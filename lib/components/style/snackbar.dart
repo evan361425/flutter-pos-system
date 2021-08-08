79561ec091fbd2b1e0c4e1bbe869e902ba4bdf58
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-08-08 15:47:54 +0800

    Add snackbar action to close snackbar

--- lib/components/style/snackbar.dart
+++ lib/components/style/snackbar.dart
@@ -1,38 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:possystem/constants/constant.dart';
 
-SnackBar createSnackBar(
+void showSnackBar(
+  BuildContext context,
   String message, {
   required Icon icon,
-  duration = const Duration(milliseconds: 600),
 }) {
-  return SnackBar(
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
     margin: const EdgeInsets.all(kSpacing2),
     behavior: SnackBarBehavior.floating,
-    duration: duration,
+    action: SnackBarAction(
+      label: '關閉',
+      onPressed: () => ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+    ),
     content: Row(children: [
       icon,
       const SizedBox(width: kSpacing0),
       Text(message),
     ]),
-  );
+  ));
 }
 
 void showSuccessSnackbar(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(createSnackBar(message,
+  showSnackBar(context, message,
       icon: const Icon(
         Icons.check_circle_outline_sharp,
         color: Color(0xFF64B5F6),
-      )));
+      ));
 }
 
 void showInfoSnackbar(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(createSnackBar(
-    message,
-    duration: const Duration(seconds: 3),
-    icon: const Icon(
-      Icons.info_outline,
-      color: Color(0xFF81C784),
-    ),
-  ));
+  showSnackBar(context, message,
+      icon: const Icon(
+        Icons.info_outline,
+        color: Color(0xFF81C784),
+      ));
 }
