1688a9010921f49dea52f071640cde2b0bb3425c
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-10-11 09:35:50 +0800

    Add menu_search test

--- test/ui/menu/menu_search_test.dart
+++ test/ui/menu/menu_search_test.dart
@@ -1,59 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:possystem/models/menu/catalog.dart';
 import 'package:possystem/models/menu/product.dart';
+import 'package:possystem/models/menu/product_ingredient.dart';
+import 'package:possystem/models/menu/product_quantity.dart';
+import 'package:possystem/models/repository/menu.dart';
+import 'package:possystem/models/stock/ingredient.dart';
+import 'package:possystem/models/stock/quantity.dart';
 import 'package:possystem/routes.dart';
 import 'package:possystem/ui/menu/menu_search.dart';
 
-import '../../mocks/mock_models.mocks.dart';
-import '../../mocks/mock_repos.dart';
+import '../../mocks/mock_storage.dart';
 
 void main() {
   testWidgets('should update products when typing', (tester) async {
-    when(menu.searchProducts(
-      limit: anyNamed('limit'),
-      text: argThat(isNull, named: 'text'),
-    )).thenReturn([Product(name: 'p1'), Product(name: 'p2')]);
+    final ingredient1 = ProductIngredient(
+      id: 'pi-1',
+      ingredient: Ingredient(id: 'i-1', name: 'i-1'),
+    );
+    final ingredient2 = ProductIngredient(
+      id: 'pi-2',
+      ingredient: Ingredient(id: 'i-2', name: 'i-2'),
+    );
+    final quantity = ProductQuantity(
+      id: 'pq-1',
+      ingredient: ingredient1,
+      quantity: Quantity(id: 'q-1', name: 'q-1'),
+    );
+    final catalog = Catalog(id: 'c-1', products: {
+      'p-1': Product(id: 'p-1', name: 'p-1', ingredients: {
+        'pi-1': ingredient1,
+        'pi-2': ingredient2,
+      }),
+      'p-2': Product(id: 'p-2', name: 'p-2'),
+    });
+    final menu = Menu();
+    ingredient1.replaceItems({'pq-1': quantity});
+    catalog.items.forEach((e) => e.catalog = catalog);
+    menu.replaceItems({'c-1': catalog});
 
     await tester.pumpWidget(MaterialApp(
-      routes: {Routes.menuProduct: (_) => Container()},
+      routes: {
+        Routes.menuProduct: (context) {
+          final arg = ModalRoute.of(context)!.settings.arguments;
+          return Text('navigated-$arg');
+        }
+      },
       home: MenuSearch(),
     ));
 
-    expect(find.text('p1'), findsOneWidget);
+    expect(find.text('p-1'), findsOneWidget);
+    expect(find.text('p-2'), findsOneWidget);
 
-    // now text some value
-
-    // empty result
-    when(menu.searchProducts(
-      limit: anyNamed('limit'),
-      text: argThat(equals('empty'), named: 'text'),
-    )).thenReturn([]);
+    // enter non-matched products
     await tester.enterText(find.byType(TextField), 'empty');
     await tester.pump();
 
-    // finds result
-    final product = MockProduct();
-    when(menu.searchProducts(
-      limit: anyNamed('limit'),
-      text: argThat(equals('some-value'), named: 'text'),
-    )).thenReturn([product]);
-    when(product.name).thenReturn('p3');
+    expect(find.text('搜尋不到相關資訊，打錯字了嗎？'), findsOneWidget);
 
-    await tester.enterText(find.byType(TextField), 'some-value');
+    // enter match products (including ingredient)
+    await tester.enterText(find.byType(TextField), '2');
     await tester.pump();
 
-    expect(find.text('p1'), findsNothing);
-    expect(find.text('p3'), findsOneWidget);
+    expect(find.text('p-1'), findsOneWidget);
+    expect(find.text('p-2'), findsOneWidget);
 
-    await tester.tap(find.text('p3'));
+    // should match specific quantity
+    await tester.enterText(find.byType(TextField), 'q-1');
+    await tester.pump();
+
+    expect(find.text('p-1'), findsOneWidget);
+    expect(find.text('p-2'), findsNothing);
+
+    // navigate to product
+    await tester.tap(find.text('p-1'));
     await tester.pumpAndSettle();
 
+    expect(find.text('navigated-p-1'), findsOneWidget);
+
     // should update searchedAt
-    verify(product.searched());
+    verify(storage.set(
+      any,
+      argThat(predicate<Map>((data) => data.values.first > 0)),
+    ));
   });
 
   setUpAll(() {
-    initializeRepos();
+    initializeStorage();
   });
 }
