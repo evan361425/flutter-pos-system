4e1cca76aa6a2f570355a5c0751352b4c7b19a7d
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-12-11 14:40:46 +0800

    Remove unused package

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:possystem/constants/constant.dart';
 import 'package:possystem/constants/icons.dart';
 
--- lib/ui/order/widgets/order_product_list.dart
+++ lib/ui/order/widgets/order_product_list.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:possystem/constants/constant.dart';
 import 'package:possystem/models/menu/catalog.dart';
