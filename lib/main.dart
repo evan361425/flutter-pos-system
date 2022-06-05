a5b17676b66320f05fd1b31b7168ac7c119b0174
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2022-06-05 17:33:13 +0800

    fix: rename firebase option

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:possystem/debug/setup_menu.dart';
-import 'package:possystem/firebase_options.dart';
+import 'package:possystem/firebase_custom_options.dart';
 import 'package:provider/provider.dart';
 
 import 'models/repository/cashier.dart';
