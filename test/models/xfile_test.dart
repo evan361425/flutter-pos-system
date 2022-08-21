f48fa486ef07de0e7423832caeba5c2e1b9cbf6d
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2022-08-21 15:38:16 +0800

    Fix XFile test for each platform

--- test/models/xfile_test.dart
+++ test/models/xfile_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:possystem/models/xfile.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:path_provider_platform_interface/src/method_channel_path_provider.dart';
 
 import '../test_helpers/file_mocker.dart';
 
@@ -10,9 +11,11 @@ void main() {
   group('XFile', () {
     testWidgets('#getRootPath', (tester) async {
       XFile.fs = const LocalFileSystem();
+      final provider = MethodChannelPathProvider();
+      PathProviderPlatform.instance = provider;
 
       tester.binding.defaultBinaryMessenger.setMockMethodCallHandler(
-        (PathProviderPlatform.instance as dynamic).methodChannel,
+        provider.methodChannel,
         (MethodCall methodCall) {
           expect(methodCall.method, 'getApplicationDocumentsDirectory');
           return Future.value('');
