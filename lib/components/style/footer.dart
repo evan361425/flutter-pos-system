add4c229797a5fed6da0754289113019c1ee151e
Lu Shueh Chou <wu0dj2k7ao3@gmail.com>
2024-07-25 11:26:41 +0800

    fix lint

--- lib/components/style/footer.dart
+++ lib/components/style/footer.dart
@@ -3,7 +3,7 @@ import 'package:possystem/components/linkify.dart';
 import 'package:possystem/components/meta_block.dart';
 
 class Footer extends StatelessWidget {
-  const Footer({Key? key}) : super(key: key);
+  const Footer({super.key});
 
   @override
   Widget build(BuildContext context) {
