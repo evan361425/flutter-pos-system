e5ff006c1f9bd31bc401b9a70d0bea6f63aadd7e
Lu Shueh Chou <wu0dj2k7ao3@gmail.com>
2024-07-28 12:50:17 +0800

    fix from production

--- lib/translator.dart
+++ lib/translator.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
 
-late AppLocalizations S;
+AppLocalizations S = AppLocalizationsEn();
