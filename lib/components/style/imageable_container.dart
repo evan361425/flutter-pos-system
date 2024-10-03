075945133c04921ecb87c2d42bce1e8b81e7c379
Lu Shueh Chou <wu0dj2k7ao3@gmail.com>
2024-10-03 21:02:58 +0800

    add invert

--- lib/components/style/imageable_container.dart
+++ lib/components/style/imageable_container.dart
@@ -44,38 +44,64 @@ class ImageableController {
   final GlobalKey key;
 
   /// How many pixels in a row
-  final double rowSize;
+  final int width;
 
   ImageableController({
     required this.key,
-    this.rowSize = 384,
+    this.width = 384,
   });
 
-  Future<Uint8List?> toImage() async {
+  Future<Uint8List?> toImage({
+    bool invertBits = false,
+  }) async {
     final boundary = key.currentContext?.findRenderObject() as RenderRepaintBoundary?;
     if (boundary == null) {
       return null;
     }
 
-    final image = await boundary.toImage(pixelRatio: rowSize / boundary.paintBounds.width);
+    final image = await boundary.toImage(pixelRatio: width / boundary.paintBounds.width);
     final byteData = await image.toByteData(format: ImageByteFormat.rawRgba);
-    final bytes = byteData?.buffer.asUint8List();
-    final result = _luminosityWeighted(bytes!);
-    print('get image size: ${result.length} if width is $rowSize, the height is ${result.length / rowSize}');
+    final bytes = byteData!.buffer.asUint8List();
+    final result = _luminosityWeighted(bytes, invertBits);
 
     return result;
   }
 
-  /// see: https://en.wikipedia.org/wiki/Luma_%28video%29#Rec._601_luma_versus_Rec._709_luma_coefficients
-  /// bit map should follow floyd-steinberg
-  Uint8List _luminosityWeighted(Uint8List bytes) {
-    final size = bytes.length ~/ 4;
-    final result = Uint8List(size);
-    for (var i = 0; i < size; i += 4) {
+  /// First convert to gray-scale then convert to binary image
+  /// gray-scale see: https://en.wikipedia.org/wiki/Luma_%28video%29#Rec._601_luma_versus_Rec._709_luma_coefficients
+  /// floyd-steinberg dithering see: https://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
+  Uint8List _luminosityWeighted(Uint8List bytes, bool invertBits) {
+    // convert to gray-sale (4 bytes to 1 byte)
+    final gray = Uint8List(bytes.length ~/ 4);
+    for (var i = 0; i < bytes.length; i += 4) {
       final r = bytes[i];
       final g = bytes[i + 1];
       final b = bytes[i + 2];
-      result[i ~/ 4] = (r * 0.299 + g * 0.587 + b * 0.114).round();
+      gray[i] = (r * 0.299 + g * 0.587 + b * 0.114).round();
+    }
+    // binary image (byte to bit)
+    final result = Uint8List(gray.length ~/ 8);
+    final lastRow = gray.length - width;
+    for (var i = 0; i < gray.length; i++) {
+      // convert to binary image
+      final p = gray[i] > 127.5 ? 1 : 0;
+      final err = gray[i] - p * 255;
+      if (invertBits) {
+        result[i ~/ 8] |= p << (i % 8);
+      } else {
+        result[i ~/ 8] |= p << (7 - i % 8);
+      }
+
+      // floyd-steinberg dithering
+      if (i % width == width - 1) {
+        gray[i] += err * 7 ~/ 16;
+      }
+
+      if (i < lastRow) {
+        gray[i + width - 1] += err * 3 ~/ 16;
+        gray[i + width] += err * 5 ~/ 16;
+        gray[i + width + 1] += err ~/ 16;
+      }
     }
     return result;
   }
